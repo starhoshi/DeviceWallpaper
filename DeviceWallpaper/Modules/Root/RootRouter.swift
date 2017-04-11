864bfdc17e5d7c181344d3c8a6ce97979a6d105b
starhoshi <kensuke1751@gmail.com>
2017-04-11 15:38:28 +0900

    Show initial window.

---
+++ DeviceWallpaper/Modules/Root/RootRouter.swift
@@ -0,0 +1,21 @@
+//
+//  RootRouter.swift
+//  DeviceWallpaper
+//
+//  Created by Kensuke Hoshikawa on 2017/04/11.
+//  Copyright © 2017年 star__hoshi. All rights reserved.
+//
+
+import Foundation
+import UIKit.UIWindow
+
+protocol RootWireframe: class {
+    func presentArticlesScreen(in window: UIWindow)
+}
+
+final class RootRouter: RootWireframe {
+    func presentArticlesScreen(in window: UIWindow) {
+        window.makeKeyAndVisible()
+//        window.rootViewController = WallpaperListRouter.assembleModule()
+    }
+}
