fe181c090ba0906f0b86d2171e72f564e66aaa5b
starhoshi <kensuke1751@gmail.com>
2017-04-11 13:54:36 +0900

    add cocoapods.

---
+++ Podfile
@@ -0,0 +1,8 @@
+platform :ios, '8.0'
+
+target 'DeviceWallpaper' do
+  use_frameworks!
+
+  pod 'SwiftLint'
+end
+
--- scripts/pod_update.sh
+++ scripts/pod_update.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh -x
 
 bundle exec pod update
 # && bundle exec carthage_acknowledgements generate -o=./DeviceWallpaper/Resources/Settings.bundle/Acknowledgements.plist -p=DeviceWallpaper
