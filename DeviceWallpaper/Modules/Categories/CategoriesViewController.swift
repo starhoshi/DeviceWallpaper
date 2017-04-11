f53f2ff905901db05bbbd3f813ea94e6c2bb4d62
starhoshi <kensuke1751@gmail.com>
2017-04-11 18:42:38 +0900

    show tableview.

--- DeviceWallpaper/Modules/Categories/CategoriesViewController.swift
+++ DeviceWallpaper/Modules/Categories/CategoriesViewController.swift
@@ -9,13 +9,15 @@
 import UIKit
 
 class CategoriesViewController: UIViewController {
+    let tableView = UITableView(frame: .zero, style: .plain)
     var presenter: CategoriesPresentation!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .yellow
+        view = tableView
+        tableView.delegate = self
+        tableView.dataSource = self
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -36,5 +38,25 @@ class CategoriesViewController: UIViewController {
 }
 
 extension CategoriesViewController: CategoriesView {
+
+}
+
+extension CategoriesViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        log?.debug(indexPath)
+    }
+
+}
+
+extension CategoriesViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = "1"
+        return cell
+    }
 
 }
