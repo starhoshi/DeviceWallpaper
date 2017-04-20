a71d9d7478b44e8b178aea88917e56f05ba0f929
starhoshi <kensuke1751@gmail.com>
2017-04-20 15:58:17 +0900

    add ListPresenter spec.

---
+++ DeviceWallpaper/Modules/WallpaperList/Presenter/WallpaperListPresenterSpec.swift
@@ -0,0 +1,29 @@
+//
+//  WallpaperListPresenterSpec.swift
+//  DeviceWallpaper
+//
+//  Created by Kensuke Hoshikawa on 2017/04/20.
+//  Copyright © 2017年 star__hoshi. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import DeviceWallpaper
+
+class WallpaperListPresenterSpec: QuickSpec {
+    override func spec() {
+
+    }
+}
+
+class MockWallpaperListView: WallpaperListView {
+    
+}
+
+class MockWallpaperListUseCase: WallpaperListUseCase {
+    
+}
+
+class MockWallpaperListWireframe: WallpaperListWireframe {
+    
+}
