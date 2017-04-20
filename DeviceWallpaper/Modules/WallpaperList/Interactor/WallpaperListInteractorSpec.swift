bfd2acd1e98dfa4b4faa3c3d70ffcf8f48744aff
starhoshi <kensuke1751@gmail.com>
2017-04-20 15:35:54 +0900

    add ListInteractor spec.

---
+++ DeviceWallpaper/Modules/WallpaperList/Interactor/WallpaperListInteractorSpec.swift
@@ -0,0 +1,37 @@
+//
+//  WallpaperListInteractorSpec.swift
+//  DeviceWallpaper
+//
+//  Created by Kensuke Hoshikawa on 2017/04/20.
+//  Copyright © 2017年 star__hoshi. All rights reserved.
+//
+
+import Quick
+import Nimble
+@testable import DeviceWallpaper
+
+class WallpaperListInteractorSpec: QuickSpec {
+    override func spec() {
+        let output = MockWallpaperListInteractorOutput()
+        let interactor = WallpaperListInteractor()
+        beforeEach {
+            interactor.output = output
+        }
+        describe("fetchWallpapers()") {
+            beforeEach {
+                interactor.fetchWallpapers()
+            }
+            it("return wallpapers") {
+                expect(output.wallpapers).to(equal(WallpapersType.toArray()))
+            }
+        }
+    }
+}
+
+class MockWallpaperListInteractorOutput: WallpaperListInteractorOutput {
+    var wallpapers: [WallpapersType]?
+
+    func wallpapersFetched(_ wallpapers: [WallpapersType]) {
+        self.wallpapers = wallpapers
+    }
+}
