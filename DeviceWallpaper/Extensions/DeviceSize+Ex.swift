0d64a927cb8059319fa43b68b6808637c3e4533e
starhoshi <kensuke1751@gmail.com>
2017-05-02 16:28:53 +0900

    set Wallpapersize.

--- DeviceWallpaper/Extensions/DeviceSize+Ex.swift
+++ DeviceWallpaper/Extensions/DeviceSize+Ex.swift
@@ -9,25 +9,47 @@
 import Foundation
 import Device
 
+// iOS Quick Reference for iPhone, iPad, and Apple Watch http://iosres.com/
 extension Size {
-    var wallpaperSize: Int {
+    var wallpaperSize: (width: Int, height: Int) {
         switch self {
         case .screen3_5Inch:
-            return 0
+            return (width: 370, height: 596)
         case .screen4Inch:
-            return 0
+            return (width: 372, height: 696)
         case .screen4_7Inch:
-            return 0
+            return (width: 426, height: 804)
         case .screen5_5Inch:
-            return 0
+            return (width: 888, height: 888)
         case .screen7_9Inch:
-            return 0
+            return (width: 631, height: 631)
         case .screen9_7Inch:
-            return 0
+            return (width: 1271, height: 1271)
         case .screen12_9Inch:
-            return 0
+            return (width: 1366, height: 1366)
         case .unknownSize:
-            return 0
+            return (width: 1000, height: 1000)
+        }
+    }
+
+    var screenSize: (width: Int, height: Int) {
+        switch self {
+        case .screen3_5Inch:
+            return (width: 320, height: 480)
+        case .screen4Inch:
+            return (width: 320, height: 568)
+        case .screen4_7Inch:
+            return (width: 375, height: 667)
+        case .screen5_5Inch:
+            return (width: 414, height: 736)
+        case .screen7_9Inch:
+            return (width: 768, height: 1024)
+        case .screen9_7Inch:
+            return (width: 768, height: 1024)
+        case .screen12_9Inch:
+            return (width: 1024, height: 1366)
+        case .unknownSize:
+            return (width: 800, height: 800)
         }
     }
 }
